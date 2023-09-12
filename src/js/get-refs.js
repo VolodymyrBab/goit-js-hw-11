171d1520ec3139c8ba49a5ac62fe121ee5b230df
VolodymyrBab <volbv@yahoo.com>
2023-09-12 18:48:37 +0200

    1

--- src/js/get-refs.js
+++
@@ -1,8 +0,0 @@
-export default function getRefs() {
-    return {
-      form: document.querySelector('#search-form'),
-      input: document.querySelector('.input'),
-      gallery: document.querySelector('.gallery'),
-      btnLoadMore: document.querySelector('.load-more'),
-    }
-  }
